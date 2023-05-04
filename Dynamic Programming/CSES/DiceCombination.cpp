430a3ece4dcb070576d2acf7d410ee0d6a8a3495
Sheikh Md. Mahedi Hasan <78357062+Mahedi-Hasan10@users.noreply.github.com>
2023-05-04 10:24:35 +0600

    Add files via upload

---
+++ Dynamic Programming/CSES/DiceCombination.cpp
@@ -0,0 +1,42 @@
+/*
+     Author : Mahedi-Hasan10
+     Date : 03-05-2023
+     Topic:
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e6 + 5;
+const int mod = 1e9 + 7;
+int dp[N];
+
+int solve(int n)
+{
+    if (dp[n] != 0)
+    {
+        return dp[n];
+    }
+
+    for (int i = 1; i <= 6; i++)
+    {
+        if (n - i >= 0)
+        {
+            dp[n] += solve(n - i);
+            dp[n] %= mod;
+        }
+    }
+    return dp[n];
+}
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= 6; i++)
+    {
+        dp[i] = pow(2, i - 1);
+    }
+    cout << solve(n) << "\n";
+
+    return 0;
+}
